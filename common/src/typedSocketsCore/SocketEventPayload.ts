b6ef4dba15c61e87630c6b62240d5d597b8dcae4
S. Tarık Çetin <cetinsamedtarik@gmail.com>
2021-01-20 05:58:07 +0300

    defined a better type for SocketEventPayload

--- common/src/typedSocketsCore/SocketEventPayload.ts
+++ common/src/typedSocketsCore/SocketEventPayload.ts
@@ -1,1 +1,7 @@
-export interface SocketEventPayload {}
+/*
+ * Reason for rule exception: `unknown` doesn't behave, so we have to use `any` here.
+ * See: https://github.com/Microsoft/TypeScript/issues/15300#issuecomment-702872440
+ * - Tarık, 2021-01-20 05:45
+ */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type SocketEventPayload = Readonly<Record<string, any>>;
